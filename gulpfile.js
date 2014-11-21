026053294f022c82f50e380e9222e7ef946b3ba0
Marco Au <marco.au.321@gmail.com>
2014-11-21 21:48:19 +0800

    restructure files into /app

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,22 @@ var sass = require('gulp-sass');
 // SERVE TASKS //
 gulp.task('connect', function() {
   connect.server({
-    root: './',
+    root: './app',
     port: 9999,
     livereload: {port: 30000}
   });
 });
 gulp.task('watch', function() {
-  gulp.watch('index.html', ['reload'])
-  gulp.watch('app.scss', ['sass']);
+  gulp.watch('./app/index.html', ['reload'])
+  gulp.watch('./app/app.scss', ['sass']);
 });
 gulp.task('reload', function() {
-  gulp.src('./').pipe(connect.reload());
+  gulp.src('./app').pipe(connect.reload());
 });
 gulp.task('sass', function() {
-  gulp.src('app.scss') // create new css file
+  gulp.src('./app/app.scss') // create new css file
     .pipe(sass({errLogToConsole: true}))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./app/'))
 });
 
 gulp.task('serve', ['sass', 'connect', 'watch']);
