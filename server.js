c28d817e72293518cff77ac484c330dbc14f3bad
Marco Au <marco.au.321@gmail.com>
2014-11-25 23:30:29 +0800

    add signup user saving and google analytics

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var favicon = require('static-favicon');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/cfu';
+mongoose.connect(mongo_uri);
+var Schema = mongoose.Schema;
+var userSchema = new Schema({}, {strict: false});
+var User = mongoose.model('User', userSchema);
+
 var app = express();
 app.use(bodyParser());
 app.use(express.static('./app'));
@@ -13,6 +20,17 @@ app.use(function(req, res, next) {
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/app/index.html');
+});
+
+app.post('/signup', function(req, res) {
+  var user = req.body.user;
+  new User(user).save(function(err, data) {
+    if(err) {
+      req.sendStatus(500);
+    } else {
+      res.send({success: true});      
+    }
+  });
 });
 
 app.listen(process.env.PORT || 9999);
